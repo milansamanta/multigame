16604b448c6c5551e9a46b52c6dab8f8dcec27f7
Milan Kumar Samanta <milansamanta6297@gmail.com>
2025-07-28 01:20:52 +0530

    digit predictor

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request, jsonify
 import tictactoe as ttt
 from tttai import AI
+from digits import predict_digit
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/tictactoe")
 def tictactoe():
@@ -31,3 +35,33 @@ def play_tictactoe():
         "player": new_player,
         "winner": winner,
     }), 200
+
+
+@app.route("/nim")
+def nim():
+    return render_template("nim.html")
+
+
+@app.route("/minesweeper")
+def minesweeper():
+    return render_template("minesweeper.html")
+
+
+@app.route("/digits")
+def digits():
+    return render_template("number_ai.html")
+
+
+@app.route("/predict", methods=["POST"])
+def get_prediction():
+    data = request.get_json()
+    if not data or "pixels" not in data:
+        return jsonify({"error": "No image provided"}), 400
+
+    image_data = data["pixels"]
+    try:
+        prediction = predict_digit(image_data)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+    return jsonify({"prediction": prediction}), 200
