840fe1bccc9fce2435ef6c3b074fbf7798e3c649
Milan Kumar Samanta <milansamanta6297@gmail.com>
2025-07-28 01:40:25 +0530

    added ai again

---
+++ handwriting.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+
+IMAGE_WIDTH = 28
+IMAGE_HEIGHT = 28
+NUM_CLASSES = 10
+
+def main():
+    mnist = tf.keras.datasets.mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_test, x_train = x_test / 255.0, x_train / 255.0
+    y_train = tf.keras.utils.to_categorical(y_train)
+    y_test = tf.keras.utils.to_categorical(y_test)
+    x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], x_train.shape[2], 1)
+    x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2], 1)
+
+    model = get_model()
+    model.fit(x_train, y_train, epochs=10)
+
+    model.evaluate(x_test,  y_test, verbose=2)
+
+    model.save("model.h5")
+
+def get_model():
+    """
+    Returns a compiled neural network model.
+    """
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(IMAGE_WIDTH, IMAGE_HEIGHT, 1)),
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(256, activation='relu'),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(256, activation='relu'),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(256, activation='relu'),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(NUM_CLASSES, activation='softmax')
+    ])
+
+    model.compile(optimizer='rmsprop',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy', 'categorical_accuracy'])
+    
+    return model
+
+if __name__ == "__main__":
+    main()
